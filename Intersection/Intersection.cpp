77e8c4781ebe8bc1488fd6a4f220471798b465de
Vladislav <u197456@mail.ru>
2022-04-20 23:07:56 +0300

    Some comments

--- Intersection/Intersection.cpp
+++ Intersection/Intersection.cpp
@@ -11,9 +11,9 @@ int main()
     Line line1 = { point1, point2 };
     Line line2 = { point3, point4 };
 
-    std::cout << hasIntersection<float>(line1, line2) << "\n";
-    std::cout << hasIntersection<float>(Line{1, 2, 5, 7}, Line {3, 3, 4, 5} ) << "\n";
-    std::cout << hasIntersection<float>(Line{ 1, -2, 1, 2 }, Line{ 1, 2, -1, 2 }) << "\n";
-    std::cout << hasIntersection<float>(Line{ 1, 2, -1, 0 }, Line{ 1, -2, -1, -2 }) << "\n";
+    std::cout << hasIntersection(line1, line2) << "\n";
+    std::cout << hasIntersection(Line{1, 2, 5, 7}, Line {3, 3, 4, 5} ) << "\n";
+    std::cout << hasIntersection(Line{ 1, -2, 1, 2 }, Line{ 1, 2, -1, 2 }) << "\n";
+    std::cout << hasIntersection<(Line{ 1, 2, -1, 0 }, Line{ 1, -2, -1, -2 }) << "\n";
 }
 
--- Intersection/Line.h
+++ Intersection/Line.h
@@ -13,7 +13,7 @@ struct Line
     Point end{};
 };
 
-template <typename T>
+template <typename T = float>
 constexpr bool hasIntersection(const Line& line1, const Line& line2) {
     
     T dX1 = line1.begin.x - line1.end.x;
@@ -22,21 +22,28 @@ constexpr bool hasIntersection(const Line& line1, const Line& line2) {
     if (!dX1 || !dY1)
         return false;
     T k1 = dY1 / dX1;
-    //
+    //y1 = k1 * x1 + y01  - equation of line
     T dX2 = line2.begin.x - line2.end.x;
     T dY2 = line2.begin.y - line2.end.y;
-    //
+    
     if (!dX2 || !dY2)
         return false;
-    //
+    //y2 = k2 * x + y02
     T k2 = dY2 / dX2;
-
+    //line are parellel or the same
     if (k1 == k2)
         return false;
+
+    //-y01 + y02 + k1 * x01 - k2*x02
+    //------------------------------ = x_ - point of intersection
+    //          k1 - k2
 
     T x_ = (-line1.begin.y + line2.begin.y + line1.begin.x * k1 - line2.begin.x * k2) / (k1 - k2);
     T y_ = line1.begin.y + x_ - line1.begin.x * k1;
 
+    //if x_  belong{line1.begin.x, line1.end.x, line2.begin.x, line2.end.x}
+    //
+    //&& y_ belong {line1.begin.y, line1.end.y, line2.begin.y, line2.end.y}
     return   x_ <= std::max(line1.begin.x, line1.end.x) && x_  >= std::max(line2.begin.x, line2.end.x) &&
              y_  <= std::max(line1.begin.y, line1.end.y) && y_ >= std::max(line2.begin.y, line2.end.y);
 
